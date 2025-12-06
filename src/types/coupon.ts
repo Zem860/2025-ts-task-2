9811675a244dc7d157a72d08a5f38d79c64db7e6
Zem860 <zemmyhammy@gmail.com>
2025-12-06 16:48:09 +0800

    修正優惠券bbug

--- src/types/coupon.ts
+++ src/types/coupon.ts
@@ -1,34 +1,38 @@
 export type CouponData = {
-    id:string,
-    code:string,
-    discount:number,
-    is_enabled:boolean
-    start_at?:number,
-    end_at?:number
+  id: string
+  code: string
+  title: string
+  percent: number
+  is_enabled: number
+  due_date: number
+  num: number
 }
 
-export type CreateCouponParams = Omit<CouponData, 'id'>
+export type Pagination = {
+  total_pages: number
+  current_page: number
+  has_pre: boolean
+  has_next: boolean
+  category: string
+}
+
+export type CreateCouponParams = Omit<CouponData, 'id' | 'num'>
 
 export type EditCouponParams = {
-    id:string,
-    data:CreateCouponParams,
+  id: string
+  data: CreateCouponParams
 }
 
 export type GetCoupounResponse = {
-    success:boolean,
-    coupons:CouponData[],
-    pagination:{
-        total_pages:number,
-        current_page:number,
-        has_pre:boolean,
-        has_next:boolean
-    }
-
+  success: boolean
+  coupons: CouponData[]
+  pagination: Pagination
+  messages: unknown[]
 }
 
 export type MessageResponse = {
-    success:boolean,
-    message:string,
+  success: boolean
+  message: string
 }
 
 export type CreateCouponResponse = MessageResponse
